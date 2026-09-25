1dac0ee029029c3c87986e00123e43b9574c4822
KD-kaustubh <ram.shivaay100@gmail.com>
2026-09-25 11:37:07 +0530

    added sqlite persistance for notes

--- server.py
+++ server.py
@@ -1,7 +1,22 @@
+import sqlite3
+
 from mcp.server import MCPServer
 
 mcp = MCPServer("My First MCP Server")
 
+conn = sqlite3.connect("notes.db")
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS notes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    content TEXT NOT NULL
+)
+""")
+
+conn.commit()
+conn.close()
 
 @mcp.tool()
 def say_hello(name: str) -> str:
@@ -13,34 +28,74 @@ def say_hello(name: str) -> str:
 def add_numbers(a: int, b: int) -> int:
     """Add two numbers together."""
     return a + b
-notes = []
 
 
 @mcp.tool()
 def add_note(title: str, content: str) -> str:
     """Add a new note."""
-    notes.append({
-        "title": title,
-        "content": content
-    })
+
+    conn = sqlite3.connect("notes.db")
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "INSERT INTO notes (title, content) VALUES (?, ?)",
+        (title, content)
+    )
+
+    conn.commit()
 
     return f"Note '{title}' added successfully."
 
 @mcp.tool()
 def list_notes() -> list:
     """Return all saved notes."""
-    return notes
+
+    conn = sqlite3.connect("notes.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT id, title, content FROM notes")
+    rows = cursor.fetchall()
+
+    conn.close()
+
+    return [
+        {
+            "id": row[0],
+            "title": row[1],
+            "content": row[2]
+        }
+        for row in rows
+    ]
 
 @mcp.tool()
 def search_notes(keyword: str) -> list:
     """Search notes by title or content."""
-    keyword = keyword.lower()
+
+    conn = sqlite3.connect("notes.db")
+    cursor = conn.cursor()
+
+    keyword = f"%{keyword}%"
+
+    cursor.execute(
+        """
+        SELECT id, title, content
+        FROM notes
+        WHERE title LIKE ? OR content LIKE ?
+        """,
+        (keyword, keyword)
+    )
+
+    rows = cursor.fetchall()
+
+    conn.close()
 
     return [
-        note
-        for note in notes
-        if keyword in note["title"].lower()
-        or keyword in note["content"].lower()
+        {
+            "id": row[0],
+            "title": row[1],
+            "content": row[2]
+        }
+        for row in rows
     ]
 
 if __name__ == "__main__":
